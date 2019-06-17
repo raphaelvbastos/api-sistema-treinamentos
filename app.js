9b36b26413b1b90156fa8e6d80c8b260e0e59cf3
raphael <raphaelvbastos@gmail.com>
2019-06-17 12:58:32 -0300

    teste

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.write("TESTE");
+    res.send('teste');
 });
 
 var porta = process.env.PORT || 8080;
