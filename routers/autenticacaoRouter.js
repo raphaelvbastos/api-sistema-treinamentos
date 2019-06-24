d73ecc0cee853ae9b0a5430d371cf0d2d9e70b10
raphael <raphaelvbastos@gmail.com>
2019-06-24 12:28:28 -0300

    cript

--- routers/autenticacaoRouter.js
+++ routers/autenticacaoRouter.js
@@ -7,8 +7,7 @@ var CriptografiaModel = require("../models/criptografiamodel");
 autenticacaoRouter.post('/', function (req, res) {
     var criptografia = new CriptografiaModel();
     UsuarioModel.findOne({
-        email: req.body.email,
-        senha: criptografia.criptografar(req.body.senha)
+        email: req.body.email
     }, (err, usuario) => {
         if (err) {
             res.status(500).json({ error: err.message });
@@ -16,7 +15,11 @@ autenticacaoRouter.post('/', function (req, res) {
             return;
         }
 
-        return  res.json(usuario);
+        if (criptografia.comparar(req.body.senha, usuario.senha)) {
+            return res.json(usuario);
+        } else {
+            return res.json(null);
+        }
     });
 });
 
