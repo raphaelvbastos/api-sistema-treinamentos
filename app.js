73d18f1a46a0246c429493ce38356cb4d087bde1
raphael <raphaelvbastos@gmail.com>
2019-07-30 12:17:16 -0300

    upload

--- app.js
+++ app.js
@@ -7,6 +7,19 @@ mongoose.set('useFindAndModify', false);
 var app = express();
 var bodyParser = require('body-parser');
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.use(bodyParser.json({limit: '500mb', extended: true}));
+app.use(bodyParser.urlencoded({
+    extended: true,
+    limit: '500mb'
+}));
+
 // app.use(require('connect').bodyParser());
 // app.use(express.bodyParser());
 
@@ -31,18 +44,7 @@ var videoModel = require("./models/videomodel");
 
 var AtualizarModelos = require("./models/atualizarmodelos");
 
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
 
-app.use(bodyParser.json({limit: '500mb', extended: true}));
-app.use(bodyParser.urlencoded({
-    extended: true,
-    limit: '500mb'
-}));
 
 // Rotas descendentes
 var autenticacaoRouter = require('./routers/autenticacaoRouter');
