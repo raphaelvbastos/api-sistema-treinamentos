aa5caa75dcb767e6b61c7a9ada73d8f405088c21
raphael <raphaelvbastos@gmail.com>
2019-07-31 10:30:46 -0300

    multer

--- routers/uploadRouter.js
+++ routers/uploadRouter.js
@@ -5,6 +5,35 @@ const multipart = require('connect-multiparty');
 
 const multipartMiddleware = multipart({ uploadDir: './arquivos' });
 
+var multer = require('multer');
+
+var storage = multer.diskStorage({ //multers disk storage settings
+    destination: function (req, file, cb) {
+        cb(null, './arquivos/');
+    },
+    filename: function (req, file, cb) {
+        var datetimestamp = Date.now();
+        cb(null, file.fieldname + '-' + datetimestamp + '.' + file.originalname.split('.')[file.originalname.split('.').length - 1]);
+    }
+});
+
+var upload = multer({ //multer settings
+    storage: storage
+}).single('file');
+
+/** API path that will upload the files */
+uploadRouter.post('/mult', function (req, res) {
+    upload(req, res, function (err) {
+        if (err) {
+            res.json({ error_code: 1, err_desc: err });
+            return;
+        }
+        res.json({ error_code: 0, err_desc: null });
+    });
+});
+
+
+
 uploadRouter.post('/teste', multipartMiddleware, (req, res) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
